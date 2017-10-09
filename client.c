49096d7751dea36ea3f7d89dd07cb7414de8ddb2
James McCarthy Marvin <jmarvin1@student00.cse.nd.edu>
2017-10-09 18:23:07 -0400

    creted client file with necessary pound includes

---
+++ client.c
@@ -0,0 +1,16 @@
+//James Marvin, Michael Burke, William Thiesen
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#define BUFFER 8128
+
+int main(int argc, char * argv[])
+{
+
+
+}
+
