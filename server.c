dd5f0b219830f8470c0597605c900217630a5aab
James McCarthy Marvin <jmarvin1@student00.cse.nd.edu>
2017-10-09 22:01:55 -0400

    updated server.c

--- server.c
+++ server.c
@@ -7,7 +7,7 @@
 #include <netinet/in.h>
 #include <netdb.h>
 //Ports we can use 41028, 41029, 41030
-#define MAX_PENDING 2
+#define MAX_PENDING 10
 #define BUFFER 8128
 int main(int argc, char *argv[])
 {
@@ -33,6 +33,54 @@ int main(int argc, char *argv[])
 	sin.sin_addr.s_addr=INADDR_ANY;
 	sin.sin_port=htons(port);
 	
+	//create socket
+	if((s=socket(PF_INET, SOCK_STREAM,0)<0))
+	{
+		perror("error creating socket");
+		exit(1);
+	}
 	
+	//make socket reusable 
+	if((setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char*)& opt,sizeof(int)))<0)
+	{
+		perror("error setting sock to reusable");
+		exit(1);
+	}
 	
+	//bind the socket to the specific address
+	if(bind(s,(struct sockaddr *)&sin, sizeof(sin))<0)
+	{
+		perror("error binding socket");
+		exit(1);
+	}
+	
+	//start listening
+	if((listen(s, MAX_PENDING))<0)	
+	{
+		perror("error listening");
+		exit(1);
+	}
+
+	while(1)
+	{
+		//accept the client
+		if((s2=accept(s, (struct sockaddr *)&sin, &len))<0)
+		{
+			perror("error accepting");
+			exit(1);
+		}
+		
+		if((len=recv(s2, buf, sizeof(buf), 0))==-1)
+		{
+			perror("error receiving message");
+			exit(1);
+		}
+		if(len==0)
+		{
+			break;
+		}
+		printf("%s\n",buf);
+		close(s2);
+	}
+		
 }
