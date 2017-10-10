ed9f2714e3cc385efc20faf8ce079c979a608078
wtheisen <wtheisen@nd.edu>
2017-10-10 11:52:43 -0400

    Changes to server and makefile

--- server.c
+++ server.c
@@ -13,6 +13,45 @@
 //Ports we can use 41028, 41029, 41030
 #define MAX_PENDING 10
 #define BUFFER 8128
+
+enum action {
+    DWLD,
+    UPLD,
+    DELF,
+    LIST,
+    MDIR,
+    RDIR,
+    CDIR,
+    QUIT
+};
+
+int download(char* fname, int fsize)
+{
+
+    //check to see if file exists
+    if(access(fname, F_OK) !=-1)
+    {
+        //file exists
+        struct stat st;
+        stat(fname, &st);
+        fsize = st.st_size;
+    }
+    else
+    {
+        fsize = -1;
+    }
+
+    char* message;
+    sprintf(message,"%d\n",fsize);
+
+    return 1;
+}
+
+int upload(char*  fname, int fsize)
+{
+    return 1;
+}
+
 int main(int argc, char *argv[])
 {
 	//create data structure
@@ -21,7 +60,7 @@ int main(int argc, char *argv[])
 	int len;
 	int s, s2;
 	int port;
-	
+
     //handle arguments
 	if(argc==2)
 	{
@@ -31,37 +70,37 @@ int main(int argc, char *argv[])
 	{
 		fprintf(stderr, "Innappropriate amount of arguments");
 		exit(1);
-	}	
+	}
 	//prepare address
 	bzero((char*)&sin, sizeof(sin));
 	sin.sin_family= AF_INET;
 	sin.sin_addr.s_addr=INADDR_ANY;
 	sin.sin_port=htons(port);
-	
+
 	//create socket
 	if(((s=socket(PF_INET, SOCK_STREAM,0))<0))
 	{
 		perror("error creating socket");
 		exit(1);
 	}
-	
-	//make socket reusable 
+
+	//make socket reusable
 	int opt = 1;
     if((setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char*)&opt,sizeof(int)))<0)
 	{
 		perror("error setting sock to reusable");
 		exit(1);
 	}
-	
+
 	//bind the socket to the specific address
 	if(bind(s,(struct sockaddr *)&sin, sizeof(sin))<0)
 	{
 		perror("error binding socket");
 		exit(1);
 	}
-	
+
 	//start listening
-	if((listen(s, MAX_PENDING))<0)	
+	if((listen(s, MAX_PENDING))<0)
 	{
 		perror("error listening");
 		exit(1);
@@ -75,65 +114,36 @@ int main(int argc, char *argv[])
 			perror("error accepting");
 			exit(1);
 		}
-		
+
 		if((len=recv(s2, buf, sizeof(buf), 0))==-1)
 		{
 			perror("error receiving message");
 			exit(1);
 		}
+
 		if(len==0)
 		{
 			break;
 		}
-		int filesize;
 
 		char* fname;
-		
-	
-		int i;
+
 		printf("%s\n",buf);
-		char *str = strdup(buf);
-		char *token;
+
 		char *command;
-		int wc=0;
 		int32_t fsize;
-		while ((token = strsep(&str, ",")) != NULL)
-		{
-			wc++;
-			if(wc==1)
-				command=token;
-			else if(wc==2)
-				filesize=atoi(token);
-			else if(wc==3)
-				fname=token;	
-					
+
+		if(strcmp(command,"DWLD")==0) {
+            if (!download(fname, fsize)) {
+                printf("ERROR: download error\n");
+            }
+        }
+        else if(strcmp(command,"UPLD")==0) {
+            if (!upload(fname, fsize)) {
+                printf("ERROR: download error\n");
+            }
 		}
 
-		printf("%s\n",command);
-		printf("%d\n",filesize);
-		printf("%s\n",fname);
-
-		if(strcmp(command,"DWLD")==0)
-		{
-			//check to see if file exists
-			if(access(fname, F_OK) !=-1)
-			{
-				//file exists
-				struct stat st;
-				stat(fname, &st);
-				fsize = st.st_size;
-			}
-			else
-			{
-				fsize = -1;
-			}
-			char* message;
-			sprintf(message,"%d\n",fsize);
-			//send file size to the client 
-			 	
-		}						
-					
 		close(s2);
 	}
-		
 }
